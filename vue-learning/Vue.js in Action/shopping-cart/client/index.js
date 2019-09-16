f29abf82d86a276df1774b1033f3ef388048da90
timmykawhi <timmykawhi@gmail.com>
2019-09-16 23:23:11 +0800

    update vue input-number

--- vue-learning/Vue.js in Action/shopping-cart/client/index.js
+++ vue-learning/Vue.js in Action/shopping-cart/client/index.js
@@ -23,14 +23,25 @@ new Vue({
             var totalPrice = 0,
                 list = this.list;
             for (var i = 0, length = list.length; i < length; i++) {
-                totalPrice += list[i].price;
+                var item = list[i];
+                totalPrice += item.price * item.count;
             }
             return totalPrice;
         }
     },
     methods: {
-        remove: function (index) {
+        handleRemove: function (index) {
             this.list.splice(index, 1);
+        },
+        handleAdd: function (index) {
+            var item = this.list[index];
+            item.count++;
+        },
+        handleReduce: function (index) {
+            var item = this.list[index];
+            var count = item.count;
+            item.count = count > 1 ? count - 1 : 0;
         }
+
     }
 });
---
+++ vue-learning/Vue.js in Action/shopping-cart/client/input-number/input-number.js
@@ -0,0 +1,85 @@
+Vue.component('input-number', {
+    template: "\
+    <div class='input-number'>\
+    <button @click='handleAdd'>+</button>\
+     <input type='text' :value='currentValue' @change='handleInputChange'>\
+    <button @click='handleReduce'>-</button>\
+    </div>",
+    props: {
+        max: {
+            type: Number,
+            default: Infinity
+        },
+        min: {
+            type: Number,
+            default: -Infinity
+        },
+        value: {
+            type: Number,
+            default: 0
+        },
+        step: {
+            type: Number,
+            default: 1
+        },
+        decimal: {
+            type: Number,
+            default: 2
+        }
+    },
+
+    data: function () {
+        return {
+            currentValue: this.value
+        }
+    },
+
+    watch: {
+        currentValue: function (value) {
+            this.$emit('input', value);
+        },
+
+        value: function (value) {
+            this.updateValue(value);
+        }
+    },
+
+    mounted: function () {
+        this.updateValue(this.value);
+    },
+    methods: {
+        isValidNumber: function (value) {
+            var reg = /(^-?(([1-9][0-9]*)|0)\.{1}\d+$)|(^-?[1-9][0-9]*$)|(0{1}$)/;
+            return reg.test(value + '');
+        },
+
+        handleInputChange: function (e) {
+            var value = e.target.value.trim();
+            if (!this.isValidNumber(value)) {
+                e.target.value = this.currentValue;
+                return;
+            }
+            value = Number(value);
+            this.updateValue(value);
+            this.currentValue = value.toFixed(this.decimal);
+        },
+        handleAdd: function () {
+            if (this.currentValue < this.max) {
+                this.currentValue += this.step;
+            }
+        },
+        handleReduce: function () {
+            if (this.currentValue > this.min) {
+                this.currentValue -= this.step;
+            }
+        },
+        updateValue: function (value) {
+            if (value > this.max) {
+                value = this.max;
+            } else if (value < this.min) {
+                value = this.min;
+            }
+            this.currentValue = value;
+        }
+    }
+});
